b3b81432e2933b483e07e6dfd98c8827c6d5f1cd
sakorkko <samuli.korkko@gmail.com>
2020-02-08 15:32:16 +0200

    merge app.py

--- manager/app.py
+++ manager/app.py
@@ -1,24 +1,38 @@
 from flask import Flask, render_template, url_for, request, jsonify
+from flask_mysqldb import MySQL
+from statistics import mean
+
 data = {}
-data
+
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'sensorDB'
+
+mysql = MySQL(app)
 
 @app.route("/")
 def index():
-    temperature = request.args.get('value')
+    temperature = int(request.args.get('value'))
     sensor = request.args.get('sensor')
+
     if sensor in data:
         data[sensor].append(temperature)
     else:
         data[sensor] = [temperature]
 
-    if len(data[sensor]) > 59:
-        # Clean up data if errors occur
-        # Send data to cloud,   average(data[sensor])
+    if len(data[sensor]) > 5:
+        # Handle data
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO sensorTable(sensor, data) VALUES (%s, %s)", (sensor, mean(data[sensor])))
+        mysql.connection.commit()
+        cur.close()
         del data[sensor]
 
     return jsonify(data)
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
+
