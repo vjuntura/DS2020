80fe58e301a1e430780f047b6a185bb9adb3d33c
vainojuntura <vaino.juntura@outlook.com>
2020-02-13 09:06:55 +0200

    Update manager app

--- manager/app.py
+++ manager/app.py
@@ -6,7 +6,7 @@ data = {}
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'sensorDB'
